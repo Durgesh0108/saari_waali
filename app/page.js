0675717c1c0ac54976498cec6227b5f53ac9bb2a
Durgesh Prajapati <103890880+Durgesh0108@users.noreply.github.com>
2024-05-16 11:39:02 +0530

    Added name Durgesh

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <div className="h-screen">
-        <Header />
+        Durgesh
         <Navbar />
         <TabSection />
         <CollectionBlack />
